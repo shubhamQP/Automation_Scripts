17114acc2657bc5bbaf2f4f5adfdd1768b251677
shubhamchavan10395 <shubham.chavan@questionpro.com>
2021-03-29 22:27:46 +0530

    Final-Draft-1.0 301 Automation

---
+++ scripts/redirect301.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+read -p 'Source : ' redirect
+read -p 'Destination URL : ' URL
+conf_path="/home/shubhamchavan/scripts/redirect.txt"
+echo ""
+echo "checking if entry is already there in conf file"
+sleep 1
+if [[ `grep -w "$redirect\|$URL" $conf_path` ]];
+then	
+	echo""
+        echo "*********** Entry is present ***********"
+	echo""
+else
+echo ""
+echo "Destination URL : https://www.questionpro.com/$URL"
+echo "Source : location ~* /$redirect"
+echo ""
+read -p  "Press Y to Confirm : " -n 1 -r
+echo ""
+
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+
+echo "location ~* /$redirect {
+        rewrite ^/$redirect https://www.questionpro.com/$URL/ permanent;
+}" >> /home/shubhamchavan/scripts/redirect.txt
+
+if [[ `nginx -t | grep -w "successful"` ]]
+then
+	if [[ `git diff | grep -w "success"` ]]
+	then
+		echo "Restarting nginx service"
+		#systemctl restart nginx
+		echo "Commiting on to the git"
+		echo""
+		echo "Git block will run"
+		echo""
+	else
+		echo "No diffrence"
+	fi
+else
+	echo "Please check manually"
+fi
+
+fi
+fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
